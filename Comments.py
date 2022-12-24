478fca75ed80fc350fa6127b04dfb3bf7b1368a4
NUCCASJNR <adegbitealareef28@yahoo.com>
2022-12-24 04:10:39 -0800

    My first commit

---
+++ Comments.py
@@ -0,0 +1,8 @@
+# Starting to learn phyton
+print("I started learning python on the 24th of december, 2022, A day to christmas")
+
+#About myself
+print("My name is Adegbite Al-Areef Ayomipo, i'm 6fts tall")
+
+# Where i'm learning
+print("I'm currenly learning python at aptlearn.io")
